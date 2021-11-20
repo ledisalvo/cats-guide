f8af94b5b0dccb3e2deca6c69544e2f031d4c0d3
ledisalvo <ledisalvo@gmail.com>
2021-11-20 20:49:20 -0300

    Add 404 Page Not Found

---
+++ src/PageNotFound.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './PageNotFound.css';
+
+export default function PageNotFound() {
+  return (
+    <div className='page-404'>
+      <div className='outer'>
+        <div className='middle'>
+          <div className='inner'>
+            <div className='inner-circle'>
+              <i className='fa fa-home'></i>
+              <span>404</span>
+            </div>
+            <span className='inner-detail'>
+              <span className='inner-status'>Oops! You're lost</span>
+              We can not find the page you're looking for.
+              <div>
+                <Link to='/' className='btn btn-info mtl'>
+                  <i className='fa fa-home'></i>&nbsp; Return home
+                </Link>
+              </div>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
