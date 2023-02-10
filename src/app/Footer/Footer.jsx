2aa4de174a0ae0ca70415c20112c97b410b6389e
Leonardo Adrian Di Salvo <ledisalvo@gmail.com>
2023-02-10 20:17:10 -0300

    Add links to social-networks in footer

--- src/app/Footer/Footer.jsx
+++ src/app/Footer/Footer.jsx
@@ -5,26 +5,37 @@ export default function Footer() {
     <div className='container'>
       <footer className='d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top'>
         <div className='col-md-4 d-flex align-items-center'>
-          <a
-            href='#'
-            className='mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1'
-          ></a>
           <span className='text-muted'>© 2023 Company, Inc</span>
         </div>
 
         <ul className='nav col-md-4 justify-content-end list-unstyled d-flex'>
           <li className='ms-3'>
-            <a className='text-muted' href='#'>
+            <a
+              className='text-muted'
+              href='https://twitter.com/PullCommitPush'
+              target='_blank'
+              rel='noreferrer'
+            >
               Twitter
             </a>
           </li>
           <li className='ms-3'>
-            <a className='text-muted' href='#'>
+            <a
+              className='text-muted'
+              href='https://www.instagram.com/leonardo.a.disalvo/'
+              target='_blank'
+              rel='noreferrer'
+            >
               Instagram
             </a>
           </li>
           <li className='ms-3'>
-            <a className='text-muted' href='#'>
+            <a
+              className='text-muted'
+              href='https://github.com/ledisalvo'
+              target='_blank'
+              rel='noreferrer'
+            >
               Facebook
             </a>
           </li>
